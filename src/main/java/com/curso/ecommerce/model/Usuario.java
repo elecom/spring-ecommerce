8580ef76e17354151eb67fb174cae10a576d03f9
Dixon Acosta <dacostaelecom@gmail.com>
2024-07-14 02:10:10 -0400

    crear las clases model, mapeo clases a tablas jpa, integrado la diseño y vista

--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,7 +1,21 @@
 package com.curso.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nombre;
 	private String username;
 	private String email;
@@ -9,6 +23,12 @@ public class Usuario {
 	private String telefono;
 	private String tipo;
 	private String password;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario() {}
 	
@@ -71,6 +91,22 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
 	}
 
 	@Override
